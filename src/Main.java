de5b5459dd8836f5455c9366cfaedb3f66f5e681
Saxsan3000 <chubkoff@ya.ru>
2024-06-20 09:56:59 +0300

    Lesson3_Terms

--- src/Main.java
+++ src/Main.java
@@ -1,66 +1,95 @@
 public class Main {
     public static void main(String[] args) {
-        // Task 1
-        int age = 17;
 
-        if (age >= 18)
-        {System.out.println("Если возраст человека равен " + age + ", то он совершеннолетний");}
+        // Task 1-2
+        byte clientOS;
+        int clientDeviceYear = 2016;
+        clientOS = 0; // iOS
+        // clientOS = 1; // Android
+        if (clientDeviceYear > 2015) {
+            if (clientOS == 0) {
+                System.out.println("Установите версию приложения для iOS по ссылке");}
+            else if (clientOS == 1) {
+                System.out.println("Установите версию приложения для Android по ссылке");}
+            else {
+                System.out.println("Неизвестное устройство");}}
         else
-        {System.out.println("Если возраст человека равен "+ age + ", то он не достиг совершеннолетия, нужно немного подождать");
-        }
-
-        // Task 2
-        byte temperature = 5; //// Не выведет ни одного из значений
-        // если температура воздуха выше 5 градусов
-        if (temperature > 5) {System.out.println("На улице " + temperature + " градусов, можно идти без шапки");}
-        // если температура ниже 5 градусов
-        if (temperature < 5) {System.out.println("На улице " + temperature + " градусов, нужно надеть шапку");}
+        if (clientOS == 0) {
+            System.out.println("Установите облегченную версию приложения для iOS по ссылке");}
+        else if (clientOS == 1) {
+            System.out.println("Установите облегченную версию приложения для Android по ссылке");}
+        else {
+            System.out.println("Неизвестное устройство");}
 
         // Task 3
-        byte speed = 60; // Не выведет ни одного из значений
-        // скорость превышена, если она больше 60 км/ч
-        if (speed > 60) {System.out.println("Если скорость " + speed + ", то придется заплатить штраф");}
-        // превышения скорости нет, если она меньше 60 км/ч
-        if (speed < 60) {System.out.println("На улице " + speed + " градусов, то можно ехать спокойно");}
+        int year = 1884;
+        int begin = year - 1584;
+        float hundredOne = (float) begin % 100;
+        float hundredFour = (float) begin % 400;
+        float four = (float) begin % 4;
+
+        if (year < begin)
+        {System.out.println("Год указан меньше ввода високосного года");}
+        else
+        {if (hundredFour == 0)
+        {System.out.println(year + " является високосным");}
+        else if ((hundredOne == 0) || (four != 0))
+        {System.out.println(year + " не является високосным");}
+        else
+            System.out.println(year + " является високосным");}
 
         // Task 4
-        age = 1; // Не выведет ни одного из значений
-        if (age >= 2 && age <= 6) {System.out.println("Если возраст человека равен " + age + " то ему нужно ходить в детский сад.");}
-        if (age >= 7 && age <= 17) {System.out.println("Если возраст человека равен " + age + " то ему нужно ходить в школу.");}
+        int days = 1;
+        int deliveryDistance = 95;
+        if (deliveryDistance >= 60) {days = days++;}
+        if (deliveryDistance <= 100) {days = days++;}
+        if (deliveryDistance > 100) {days = 0;}
+
+        if (days == 0) {System.out.println("Свыше 100км доставка не производится");}
+        else {System.out.println("На доставку потребуется " + days + " дней");}
 
         // Task 5
-        age = 3;
-        if (age >= 14)
-        {System.out.println("Если возраст ребенка равен " + age + " то ему можно кататься без сопровождения взрослых.");}
-        else {
-            if (age >= 5 && age < 14) {
-                System.out.println("Если возраст ребенка равен " + age + " то ему можно кататься в сопровождении взрослых.");}
-            else
-            {System.out.println("Если возраст ребенка равен " + age + " то ему нельзя кататься.");}
-        }
-        // Task 6
-        byte sit = 102;
-        byte sitting = 60;
-        byte sittingBusy = 30;
-        byte standing = (byte) (sit - sitting);
-        byte standingBusy = 15;
-        if (sittingBusy + standingBusy >= sit)
-        {System.out.println("Вагон полон");}
-        else
-        {System.out.println("В вагоне свободно " + (sitting - sittingBusy) + " сидячих и " + (standing - standingBusy) + " стоячих мест");}
-
-        // Task 7
-        int one = 1000;
-        int two = 5500;
-        int three = 3765;
-
-        if (one > two && one > three)
-        {System.out.println(one + " больше");}
-        else
-        {if (two > three)
-        {System.out.println(two + " больше");}
-        else
-        {System.out.println(three + " больше");}
+        byte monthNumber = 12;
+        switch (monthNumber) {
+            case 1:
+                System.out.println("Январь");
+                break;
+            case 2:
+                System.out.println("Февраль");
+                break;
+            case 3:
+                System.out.println("Март");
+                break;
+            case 4:
+                System.out.println("Апрель");
+                break;
+            case 5:
+                System.out.println("Май");
+                break;
+            case 6:
+                System.out.println("Июнь");
+                break;
+            case 7:
+                System.out.println("Июль");
+                break;
+            case 8:
+                System.out.println("Август");
+                break;
+            case 9:
+                System.out.println("Сентябрь");
+                break;
+            case 10:
+                System.out.println("Октябрь");
+                break;
+            case 11:
+                System.out.println("Ноябрь");
+                break;
+            case 12:
+                System.out.println("Декабрь");
+                break;
+            default:
+                System.out.println("Такого месяца не существует");
         }
     }
+
 }
